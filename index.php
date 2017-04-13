b0158c675aeff2b4d62c06eb8d0dcea979920ff5
Carlo Fontanos <carl.fontanos@gmail.com>
2017-04-13 15:12:37 +0800

    Moved files to github

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Example</title>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+	<h1>example.com</h1>
+	<p>Resize this responsive page to see the effect!</p> 
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-offset-2 col-sm-8">
+			<div class="card">
+				<div class="dropdown card-option">	
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<span class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">HTML</a></li>
+						<li><a href="#">CSS</a></li>
+						<li><a href="#">JavaScript</a></li>
+					</ul>
+				</div>
+				<div class="card-header">
+					<h3>Column 1</h3>
+				</div>
+				<div class="card-body">
+					<p>Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse.</p>
+				</div>
+				<div class="card-footer">
+					<a href="#" class="text-uppercase text-success"><strong>Read our features</strong></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-offset-2 col-sm-8">
+			<div class="card clearfix equal">
+				<div class="col-sm-4 col bg-primary">
+					<div class="row">
+						<i class="fa fa-play-circle fa-5x center-element" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div class="col-sm-8 col">
+					<div class="row">
+						<div class="card-header">
+							<h3>Column 2</h3>
+						</div>
+						<div class="card-body">
+							<p>Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse.</p>
+						</div>
+						<div class="card-footer">
+							<a href="#" class="text-uppercase text-danger"><strong>Read our features</strong></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-offset-2 col-sm-8">
+			<div class="card">
+				<div class="dropdown card-option">	
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<span class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">HTML</a></li>
+						<li><a href="#">CSS</a></li>
+						<li><a href="#">JavaScript</a></li>
+					</ul>
+				</div>
+				<div class="card-header">
+					<h3>Column 3</h3>
+				</div>
+				<div class="card-body">
+					<p>Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse.</p>
+				</div>
+				<div class="card-footer">
+					<a href="#" class="text-uppercase text-warning"><strong>Read our features</strong></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+  
+<br />
+<br />
+
+</body>
+</html>
